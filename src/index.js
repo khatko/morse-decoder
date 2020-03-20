1fe4940b4503907303af13375b1f124d54ac5e13
Natalie <natali14441@gmail.com>
2020-03-20 12:16:19 +0300

    feat: add morse-decoder function

--- src/index.js
+++ src/index.js
@@ -38,7 +38,22 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = "";
+    let words = expr.split("**********"); 
+    words.forEach(word => { 
+        result += (result === "" ? "" : " ");
+        let letters = word.match(/.{10}/g); 
+        letters.forEach(letter => {
+            let key = "";
+            let codes = parseInt(letter).toString().match(/.{2}/g);
+            codes.forEach(code => {
+                key += (code === "10" ? "." : "-");
+            });
+        	result += MORSE_TABLE[key];
+        });
+       	
+    });
+    return result;
 }
 
 module.exports = {
